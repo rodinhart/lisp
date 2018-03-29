e0f23021052c5369500b622171aa13fd967ee205
Rodin van der Hart <rodin.hart@concentra.co.uk>
2018-03-29 16:33:38 +0100

    Added jumptable

--- c-implementation/main.c
+++ c-implementation/main.c
@@ -1,9 +1,11 @@
+// TODO split into files: read, print, eval, etc.
+
 #include "ctype.h"
 #include "stdbool.h"
 #include "stdio.h"
 #include "stdlib.h"
 
-enum { INT, SYMBOL, CONS };
+enum { INT, SYMBOL, CONS, CORE };
 
 typedef struct _cell {
   union {
@@ -113,6 +115,15 @@ cell Cons(cell first, cell rest) {
   return xs;
 }
 
+// Core :: int -> Cell
+cell Core(int i) {
+  cell x = gcAlloc();
+  x->type = CORE;
+  x->data.i = i;
+
+  return x;
+}
+
 // count :: List a -> Int
 int count(cell xs) {
   int count = 0;
@@ -150,6 +161,8 @@ void print(cell x) {
     printf("%i", x->data.i);
   } else if (x->type == SYMBOL) {
     printf("%s", x->data.s);
+  } else if (x->type == CORE) {
+    printf("FN%i", x->data.i);
   }
 }
 
@@ -225,42 +238,59 @@ bool eq(char *a, char *b) {
   return strcmp(a, b) == 0;
 }
 
+cell eval(cell, cell);
+
+cell def(cell scope, cell expr, cell args) {
+  cell value = eval(scope, first(rest(args)));
+  GC_SCOPE = Cons(first(args), Cons(value, GC_SCOPE));
+
+  return value;
+}
+
+cell mul(cell scope, cell expr, cell args) {
+  int i = 1;
+  while (args != Nil) {
+    i *= eval(scope, first(args))->data.i; // TODO check for Int
+    args = rest(args);
+  }
+
+  return Int(i);
+}
+
+cell add(cell scope, cell expr, cell args) {
+  int i = 0;
+  while (args != Nil) {
+    i += eval(scope, first(args))->data.i; // TODO check for Int
+    args = rest(args);
+  }
+
+  return Int(i);
+}
+
+cell fn(cell scope, cell expr, cell args) {
+  return expr;
+}
+
+typedef cell (*_core)(cell, cell, cell);
+_core core[] = {
+  def,
+  mul,
+  add,
+  fn
+};
+
 // eval :: List a -> Cell -> Cell
 cell eval(cell scope, cell x) {
   if (x == Nil) return Nil;
 
   if (x->type == CONS) {
     cell op = eval(scope, x->data.c.first);
+    if (op == Nil) return Nil;
+
     cell args = rest(x);
-    if (op->type == SYMBOL) {
-      char *f = op->data.s;
-      if (eq(f, "def")) {
-        cell value = eval(scope, first(rest(args)));
-        GC_SCOPE = Cons(first(args), Cons(value, GC_SCOPE));
-        return value;
-      } else if (eq(f, "*")) {
-        int i = 1;
-        while (args != Nil) {
-          i *= eval(scope, first(args))->data.i; // TODO check for Int
-          args = rest(args);
-        }
-
-        return Int(i);
-      } else if (eq(f, "+")) {
-        int i = 0;
-        while (args != Nil) {
-          i += eval(scope, first(args))->data.i; // TODO check for Int
-          args = rest(args);
-        }
-
-        return Int(i);
-      } else if (eq(f, "fn")) {
-        return x;
-      }
-
-      printf("No such operator. %s\n", f);
-      return Nil;
-    } else {
+    if (op->type == CORE) {
+      return core[op->data.i](scope, x, args);
+    } else { // TODO check op is lambda
       cell names = first(rest(op));
       while (names != Nil) {
         scope = Cons(first(names), Cons(eval(scope, first(args)), scope));
@@ -278,8 +308,8 @@ cell eval(cell scope, cell x) {
       c = rest(rest(c));
     }
 
-    // printf("No such symbol. %s\n", x->data.s);
-    return x;
+    printf("No such symbol. %s\n", x->data.s);
+    return Nil;
   }
   
   return x;
@@ -288,6 +318,12 @@ cell eval(cell scope, cell x) {
 // main :: () -> Int
 int main() {
   gcInit(200);
+  GC_SCOPE =
+    Cons(Symbol("def"), Cons(Core(0),
+    Cons(Symbol("*"), Cons(Core(1),
+    Cons(Symbol("+"), Cons(Core(2),
+    Cons(Symbol("fn"), Cons(Core(3),
+    Nil))))))));
 
   while (true) {
     printf(" => ");
